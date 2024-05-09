02fd2293dfcb98a2bf3879158572411009cdbdf4
Paola <Paola@DESKTOP-L53JRFL>
2024-05-08 22:50:29 -0300

    Adicionar arquivos de projeto.

---
+++ 1013_O_Maior_Study/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+class URI { 
+    
+    static void Main(string[] args)
+
+    {
+        string [] numeros = Console.ReadLine().Split(' ');
+
+        int A = int.Parse(numeros[0]);
+        int B = int.Parse(numeros[1]);
+        int C = int.Parse(numeros[2]);
+
+        int maiorAB = (A + B + Math.Abs(A - B)) /2;
+
+        int maiorABC = (maiorAB + C + Math.Abs(maiorAB - C)) / 2;
+
+
+        Console.WriteLine(maiorABC + " eh o maior");
+
+        Console.ReadLine();
+
+    } 
+}
